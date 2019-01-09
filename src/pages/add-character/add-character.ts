b3a8e4dab9d4b443efaa81130d80aef811b03d83
Fabiozappo <fabio.zappardino@gmail.com>
2019-01-09 11:21:21 +0100

    camera try 2

--- src/pages/add-character/add-character.ts
+++ src/pages/add-character/add-character.ts
@@ -83,5 +83,13 @@ export class AddCharacterPage {
       targetWidth: 300,
       targetHeight: 300,      
     }
+
+    this.camera.getPicture(options).then((imageData) => {
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64 (DATA_URL):
+      let base64Image = 'data:image/jpeg;base64,' + imageData;
+     }, (err) => {
+      // Handle error
+     });
   }
 }
